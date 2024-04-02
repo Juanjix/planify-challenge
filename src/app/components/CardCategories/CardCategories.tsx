40ea034e90aac67eb433c02ac8f406fb7bf0d581
Juan José Peralta <juanjose@indicius.com>
2024-04-02 15:49:48 -0300

    feat: add CardCategories component

---
+++ src/app/components/CardCategories/CardCategories.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledCardCategories = styled.div``;
+
+const CardCategories = () => {
+  return <StyledCardCategories></StyledCardCategories>;
+};
+
+export default CardCategories;
